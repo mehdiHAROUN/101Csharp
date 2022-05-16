c59a56f8c5704fbe9f611ee045f1c064565ea489
mehdiHAROUN <harounmehdi90@gmail.com>
2022-05-16 21:32:13 +0200

    singleton

---
+++ 101CSharpByExemple/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101CSharpByExemple
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton instance;
+
+        public static Singleton Instance
+        {
+            get {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+           
+        }
+
+        private int test;
+
+        public int Test
+        {
+            get { return test; }
+            set { test = value; }
+        }
+
+
+
+    }
+
+    public class Test
+    {
+        Singleton a =  Singleton.Instance;
+        int b = a.Test;
+
+    }
+}
